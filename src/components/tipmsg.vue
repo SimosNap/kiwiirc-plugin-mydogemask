3598aefad8ee8e79dc9ad94e800e821ae7785fb8
H7-25 <admin@simosnap.org>
2023-02-16 14:21:58 +0100

    Fixed tip icon height

--- src/components/tipmsg.vue
+++ src/components/tipmsg.vue
@@ -4,12 +4,14 @@
             <img src="plugins/wallet-icons/tippingjar_48.png">
         </div>
         <div v-if="!self">
-            <p>You got a Tip! <a class="kiwi-nick" :data-nick="nickname">{{ nickname }}</a> sent you {{ amount }} Ðogecoin!</p>
-            <p><a :href="dogechain" target="_blank">View Transaction</a></p>
+            You got a Tip! <a class="kiwi-nick" :data-nick="nickname">{{ nickname }}</a> sent you {{ amount }} Ðogecoin!
+            <br>
+            <a :href="dogechain" target="_blank">View Transaction</a>
         </div>
         <div v-else>
-            <p>You sent a tip of {{ amount }} Ðogecoin to <a class="kiwi-nick" :data-nick="nickname">{{ nickname }}</a>!</p>
-            <p><a :href="dogechain" target="_blank">View Transaction</a></p>
+            You sent a tip of {{ amount }} Ðogecoin to <a class="kiwi-nick" :data-nick="nickname">{{ nickname }}</a>!
+            <br>
+            <a :href="dogechain" target="_blank">View Transaction</a>
         </div>        
     </div>
 </template>
@@ -32,6 +34,7 @@ div.tip-card {
     display: flex !important;
     border: 1px solid var(--comp-border);
     border-radius: 5px;
+    line-height: 1.25em;
 }
 
 .tip-card div {
@@ -40,6 +43,7 @@ div.tip-card {
 .tip-icon {
     padding: 4px;
     width: 40px;
+    height:40px;
 }
 .tip-icon img {
     width: 32px;
